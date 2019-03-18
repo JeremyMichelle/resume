482f4798b26fdac55f556f7dbb48d764398ce0bb
vansoft2 <892550260@qq.com>
2019-03-18 21:15:52 +0800

    关于我，我的经历

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,16 @@ const app = getApp()
 Page({
   data: {
     background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
-    userInfo: {}
+    userInfo: {},
+    imgUrls: [
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
+    indicatorDots: false,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
   },
   onLoad: function () {
     var that = this
@@ -11,6 +20,7 @@ Page({
         that.setData({
           userInfo: res.userInfo
         })
+        console.log(that.data.userInfo)
       }
     })
   }
