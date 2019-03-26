afd437c0876e4457b88955bcfb353997b7d48278
vansoft2 <892550260@qq.com>
2019-03-26 18:06:02 +0800

    配置nginx服务代理，ssl证书，安装pm2的后台管理，修改页面样式，修改后台接口

--- api/app.js
+++ api/app.js
@@ -4,7 +4,7 @@ var documentRoot = 'file:///Users/gzx/resume/api/file/imgs';
 app.post('/subMessage', function (req, res) {
   let data = req.body;
   data.time = new Date().toLocaleDateString().toString();
-  let sql = `INSERT INTO  message (msg_header,msg_name,msg_time,msg_content)  VALUES("${data.header}","${data.name}","${data.time}","${data.content}")`;
+  let sql = `INSERT INTO  message (header,name,time,content)  VALUES("${data.header}","${data.name}","${data.time}","${data.content}")`;
   query(sql).then(function (data) {
     res.status(200);
     res.json(data);
--- web/etc/config.js
+++ web/etc/config.js
@@ -1,11 +1,11 @@
 export default {
   // 线上环境
-  // basePath: 'https://pro.park-api.van-soft.com',
-  // domain: 'https://pro.park-api.van-soft.com',
+  basePath: 'https://guozhuangxin.cn/',
+  // domain: 'https://guozhuangxin.cn/',
   // appid: "wx0ff7cd42eb1029f8"
   // 本地环境
-  basePath: 'http://localhost:3001/', 
-  domain: 'http://localhost:3000/', 
+  // basePath: 'http://localhost:3001/', 
+  // domain: 'https://guozhuangxin.cn/', 
   ajax: function (url,params,method) {
     params = (params == undefined ? {} : params);
     method = (method == undefined ? 'GET' : method);
@@ -39,7 +39,7 @@ export default {
     return this.ajax(url,params,method);
   },
   subMessage: function (params) {
-    let url = 'messageList';
+    let url = 'subMessage';
     let method = 'POST';
     return this.ajax(url, params, method);
   }
